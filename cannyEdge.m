54c1f05d7d9d5a31cdba89e0729243bfcd1e1eee
Joseph Trovato <josephjtrovato@gmail.com>
2014-09-12 14:33:53 -0400

    found gradient, completed non-maximum suppression, need to work on smoothing and hysterisis

--- cannyEdge.m
+++ cannyEdge.m
@@ -1,4 +1,24 @@
 % Function to compute Canny Edges
 function E = cannyEdge(I)
 
+% pad the image
+r = 2;
+Ip = padIm(I, r);
+
+% smooth the image with Gaussian Blur
+G = fspecial('gaussian', [3,3], 0.5);
+Is = conv2(Ip, G, 'same');
+
+% compute the gradient, magnitude, direction
+dx = [1 -1];
+dy = dx';
+Jx = conv2(Is, dx, 'same');
+Jy = conv2(Is, dy, 'same');
+
+J_mag = sqrt(Jx.^2 + Jy.^2)
+J_dir = atan2(Jx, Jy)
+
+E = 0;
+ 
+
 end
--- canny_test.m
+++ canny_test.m
@@ -8,8 +8,15 @@ I = [1 1 .5 1 0 .5 .5 1 0 1 0;
     .5 .5 .5 .5 .5 .5 1 1 1 0 0;
     0 0 .5 .5 0 .5 1 1 0 0 1]; 
 
+% uncomment to test on a real image
+% I = imread('test.jpg');
+% I = rgb2gray(I);
+
 E = cannyEdge(I);
 
 figure();
 imagesc(E);
+colormap('gray');
+figure();
+imagesc(I);
 colormap('gray');
