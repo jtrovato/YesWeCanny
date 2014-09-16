9d551c0ad0b4773555dcecb36f643d84ec43ecb7
Justin <justin@jyim-E6510.(none)>
2014-09-16 00:36:25 -0400

    Fixed J_dir calculation (Jy negative due to image indexing direction), adjusted thresholds, calculate hysteresis with conv2.

--- cannyEdge.m
+++ cannyEdge.m
@@ -5,7 +5,7 @@ function E = cannyEdge(I)
 nr = size(I,1);
 nc = size(I,2);
 pad = 2;
-Ip = padIm(I, pad);
+Ip = double(padIm(I, pad));
 
 % smooth the image with Gaussian Blur
 G = fspecial('gaussian', [5,5], 1.4);
@@ -30,65 +30,96 @@ Jy = conv2(Jy, S', 'same');
 
 J_mag = sqrt(Jx.^2 + Jy.^2);
 J_mag_norm = (J_mag-min(min(J_mag)))/(max(max(J_mag))-min(min(J_mag))); %normalize the magnitudes so thresholding is easier
-J_dir = atan2d(Jy, Jx);
-E_dir = atan2d(-Jx, Jy);
+J_dir = atan2d(-Jy, Jx);
+E_dir = atan2d(-Jx, -Jy);
 
+figure();
+imagesc(J_mag); colormap('gray'); axis equal
+hold on
+
+%{a
+quiver(Jx, Jy);
+hold off
+%}
 
 % Non-maximum supression: thinning out the edges
 % using an interpolation algorithm from wikipedia. It uses four points
 % instead of two closest to the gradient direction.
-E = zeros(nr+2*pad, nc+2*pad);
-for i = 2: size(J_dir, 1)-1;
-    for j = 2:size(J_dir, 2)-1;
-        theta = J_dir(i, j);
+J_mag_supp = zeros(nr+2*pad, nc+2*pad);
+for ii = 2: size(J_dir, 1)-1;
+    for jj = 2:size(J_dir, 2)-1;
+        theta = J_dir(ii, jj);
         if theta<0
             theta= theta + 360;
         end
-        mag = J_mag(i,j);
+        mag = J_mag(ii,jj);
             if (theta >= 0 && theta < 45) || (theta >= 180 && theta < 225)
-                sam1 = 0.5*(J_mag(i-1,j+1) + J_mag(i, j+1));
-                sam2 = 0.5*(J_mag(i, j-1) + J_mag(i+1, j-1));
-                E(i,j) = J_mag_norm(i,j)*(mag > sam1 && mag > sam2);
+                sam1 = 0.5*(J_mag(ii-1,jj+1) + J_mag(ii, jj+1));
+                sam2 = 0.5*(J_mag(ii, jj-1) + J_mag(ii+1, jj-1));
             elseif  (theta >= 45 && theta < 90) || (theta >= 225 && theta < 270)
-                sam1 = 0.5*(J_mag(i-1,j) + J_mag(i-1, j+1));
-                sam2 = 0.5*(J_mag(i+1, j-1) + J_mag(i+1, j));
-                E(i,j) = J_mag_norm(i,j)*(mag > sam1 && mag > sam2);
+                sam1 = 0.5*(J_mag(ii-1,jj) + J_mag(ii-1, jj+1));
+                sam2 = 0.5*(J_mag(ii+1, jj-1) + J_mag(ii+1, jj));
             elseif  (theta >= 90 && theta < 135) || (theta >= 270 && theta < 315)
-                sam1 = 0.5*(J_mag(i-1, j-1) + J_mag(i-1, j));
-                sam2 = 0.5*(J_mag(i+1, j) + J_mag(i+1, j+1));
-                E(i,j) = J_mag_norm(i,j)*(mag > sam1 && mag > sam2);
+                sam1 = 0.5*(J_mag(ii-1, jj-1) + J_mag(ii-1, jj));
+                sam2 = 0.5*(J_mag(ii+1, jj) + J_mag(ii+1, jj+1));
             elseif  (theta >= 135 && theta < 180) || (theta >= 315 && theta <= 360)
-                sam1 = 0.5*(J_mag(i-1, j-1) + J_mag(i, j-1));
-                sam2 = 0.5*(J_mag(i, j+1) + J_mag(i+1, j+1));
-                E(i,j) = J_mag_norm(i,j)*(mag > sam1 && mag > sam2);
+                sam1 = 0.5*(J_mag(ii-1, jj-1) + J_mag(ii, jj-1));
+                sam2 = 0.5*(J_mag(ii, jj+1) + J_mag(ii+1, jj+1));
             else
                 error('thats an invalid angle');
             end
+            J_mag_supp(ii,jj) = J_mag_norm(ii,jj)*(mag > sam1 && mag > sam2);
+            if (jj > 210) && (jj < 220) && (ii > 102) && (ii < 110)
+                disp([jj,ii,mag,sam1,sam2])
+                1;
+            end
     end
 end
 
-figure(); imshow(E); colormap(gray);
+figure(); imshow(J_mag_supp); colormap(gray);
 
 % hysteresis: connecting the edges
-thres_high = 0.2;
-thres_low = 0.01;
+thres_high = 0.04;
+thres_low = 0.002;
 
-for i=1:size(E, 1);
-    for j =1:size(E,2);
-        if E(i,j) > thres_high
-            E(i,j) = 1;
-        elseif E(i,j) > thres_low
-            if E(i-1:i+1,j-1:j+1)
-                E(i,j) = 1;
+J_mag_sized = J_mag_supp(3:(end-2),3:(end-2));
+E = J_mag_sized > thres_high;
+
+%figure(100)
+%ys = [];
+
+for ii = 1:numel(E)
+    low = J_mag_sized > thres_low;
+    hyst = conv2(single(E),ones(3,3),'same');
+    new = (low & hyst) & ~E;
+    %ys = [ys, sum(sum(new))];
+    if all(all(~new))
+        break
+    end
+    E = E | new;
+
+end
+
+
+%plot(1:(ii-1),ys);
+%{
+for ii=1:size(J_mag_supp, 1);
+    for jj =1:size(J_mag_supp,2);
+        if J_mag_supp(ii,jj) > thres_high
+            J_mag_supp(ii,jj) = 1;
+        elseif J_mag_supp(ii,jj) > thres_low
+            if any(J_mag_supp(ii-1:ii+1,jj-1:jj+1))
+                J_mag_supp(ii,jj) = 1;
             else
-                E(i,j) = 0;
+                J_mag_supp(ii,jj) = 0;
             end
         else
-            E(i,j) = 0;
+            J_mag_supp(ii,jj) = 0;
         end
     end
 end
-            
+%}
+         
 
 
 end
